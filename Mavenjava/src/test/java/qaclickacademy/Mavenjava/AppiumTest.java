c12ea5aa46f4eed2572c7703ec34c36cd08d4605
lakshmi <lakshmi1997re@gmail.com>
2021-06-28 13:49:52 +0530

    first commit

---
+++ Mavenjava/src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAPPAndroid() {
+		System.out.println("NativeAPPAndroid");
+	}
+	@Test
+	public void IOSAPPs() {
+		System.out.println("IOS");
+	
+	}
+}
---
+++ Mavenjava/src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+//testing xml file from maven
+	//how to execute all test cases from test folder-maven test
+	//
+	//@Test
+	public void BrowserAutomation(){
+		System.out.println("BrowserAutomation");
+
+	}
+	
+	@Test
+	public void elementsUi() {
+		System.out.println("elementsUi");
+	}
+}
